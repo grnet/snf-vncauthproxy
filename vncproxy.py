9c6144cb8e6c147acd8ea265f8e135039531e911
Apollon Oikonomopoulos <apollon@noc.grnet.gr>
2010-09-10 20:22:17 +0300

    Code cleanup/refactoring

--- vncproxy.py
+++ vncproxy.py
@@ -25,13 +25,10 @@ import logging
 import gevent
 
 import rfb
-import d3des
 
 from gevent import socket
 from gevent.select import select
 from struct import pack, unpack
-
-CONNECT_TIMEOUT = 30 
 
 class VncForwarder(gevent.Greenlet):
     """
@@ -42,14 +39,14 @@ class VncForwarder(gevent.Greenlet):
     to a specified remote host:port. Furthermore, it implements VNC
     Authentication, intercepting the client/server handshake and asking the
     client for authentication even if the backend requires none.
-    
+
     It is primarily intended for use in virtualization environments, as a VNC
     ``switch''.
 
     """
     id = 1
 
-    def __init__(self, sport, daddr, dport, password):
+    def __init__(self, sport, daddr, dport, password, connect_timeout=30):
         """
         @type sport: int
         @param sport: source port
@@ -59,6 +56,9 @@ class VncForwarder(gevent.Greenlet):
         @param dport: destination port
         @type password: str
         @param password: password to request from the client
+        @type connect_timeout: int
+        @param connect_timeout: how long to wait for client connections
+                                (seconds)
 
         """
         gevent.Greenlet.__init__(self)
@@ -68,11 +68,10 @@ class VncForwarder(gevent.Greenlet):
         self.daddr = daddr
         self.dport = dport
         self.password = password
-        #self.log = logging.getLogger('[C%d]' % self.id)
         self.log = logging
-        #self.log = DummyLogger()
         self.server = None
         self.client = None
+        self.timeout = connect_timeout
 
     def _cleanup(self):
         """Close all active sockets and exit gracefully"""
@@ -125,7 +124,22 @@ class VncForwarder(gevent.Greenlet):
 
 
     def _handshake(self):
-        """Perform handshake/authentication with a connecting client"""
+        """
+        Perform handshake/authentication with a connecting client
+
+        Outline:
+        1. Client connects
+        2. We fake RFB 3.8 protocol and require VNC authentication
+        3. Client accepts authentication method
+        4. We send an authentication challenge
+        5. Client sends the authentication response
+        6. We check the authentication
+        7. We initiate a connection with the backend server and perform basic
+           RFB 3.8 handshake with it.
+        8. If the above is successful, "bridge" both connections through two
+           "fowrarder" greenlets.
+
+        """
         self.client.send(rfb.RFB_VERSION_3_8 + "\n")
         client_version = self.client.recv(1024)
         if not rfb.check_version(client_version):
@@ -163,7 +177,8 @@ class VncForwarder(gevent.Greenlet):
 
         # Accept the authentication
         self.client.send(rfb.to_u32(rfb.RFB_AUTH_SUCCESS))
-        
+
+        # Initiate server connection
         for res in socket.getaddrinfo(self.daddr, self.dport, socket.AF_UNSPEC,
                                       socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
             af, socktype, proto, canonname, sa = res
@@ -176,7 +191,7 @@ class VncForwarder(gevent.Greenlet):
             try:
                 self.debug("Connecting to %s:%s" % sa[:2])
                 self.server.connect(sa)
-                self.debug("Success")
+                self.debug("Connection to %s:%s successful" % sa[:2])
             except socket.error, msg:
                 self.server.close()
                 self.server = None
@@ -194,7 +209,7 @@ class VncForwarder(gevent.Greenlet):
             self._cleanup()
 
         self.server.send(rfb.RFB_VERSION_3_8 + "\n")
-        
+
         res = self.server.recv(1024)
         types = rfb.parse_auth_request(res)
         if not types:
@@ -210,14 +225,16 @@ class VncForwarder(gevent.Greenlet):
             self._cleanup()
 
         self.server.send(rfb.to_u8(rfb.RFB_AUTHTYPE_NONE))
-        
+
+        # Check authentication response
         res = self.server.recv(4)
         res = rfb.from_u32(res)
 
         if res != 0:
             self.error("Authentication error")
             self._cleanup()
-            
+
+        # Bridge client/server connections
         self.workers = [gevent.spawn(self._forward, self.client, self.server),
                         gevent.spawn(self._forward, self.server, self.client)]
         gevent.joinall(self.workers)
@@ -227,27 +244,34 @@ class VncForwarder(gevent.Greenlet):
 
     def _run(self):
         sockets = []
-        for res in socket.getaddrinfo(LISTEN_ADDR, self.sport, socket.AF_UNSPEC,
+
+        # Use two sockets, one for IPv4, one for IPv6. IPv4-to-IPv6 mapped
+        # addresses do not work reliably everywhere (under linux it may have
+        # been disabled in /proc/sys/net/ipv6/bind_ipv6_only).
+        for res in socket.getaddrinfo(None, self.sport, socket.AF_UNSPEC,
                                       socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
             af, socktype, proto, canonname, sa = res
             try:
                 s = socket.socket(af, socktype, proto)
                 if af == socket.AF_INET6:
+                    # Bind v6 only when AF_INET6, otherwise either v4 or v6 bind
+                    # will fail.
                     s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 1)
             except socket.error, msg:
                 s = None
                 continue;
-            
+
             try:
                 s.bind(sa)
                 s.listen(1)
                 self.debug("Listening on %s:%d" % sa[:2])
             except socket.error, msg:
-                self.error("Error binding to %s:%d: %s" % 
+                self.error("Error binding to %s:%d: %s" %
                                (sa[0], sa[1], msg[1]))
                 s.close()
                 s = None
                 continue
+
             if s:
                 sockets.append(s)
 
@@ -256,40 +280,65 @@ class VncForwarder(gevent.Greenlet):
             self._cleanup()
 
         self.log.debug("Waiting for client to connect")
-        act, _, _ = select(sockets, [], [], timeout=CONNECT_TIMEOUT)
+        rlist, _, _ = select(sockets, [], [], timeout=self.timeout)
 
-        if not act:
-            self.info("Timed out, no connection after %d sec" % CONNECT_TIMEOUT)
+        if not rlist:
+            self.info("Timed out, no connection after %d sec" % self.timeout)
             self._cleanup()
 
-        for s in act:
-            self.client, addrinfo = s.accept()
+        for sock in rlist:
+            self.client, addrinfo = sock.accept()
             self.info("Connection from %s:%d" % addrinfo[:2])
-            for l in sockets:
-                l.close()
+
+            # Close all listening sockets, we only want a one-shot connection
+            # from a single client.
+            for listener in sockets:
+                listener.close()
             break
 
         self._handshake()
 
 
 if __name__ == '__main__':
-    LISTEN_ADDR = None
-    LISTEN_PORT = 5900
-    CTRL_SOCKET="/tmp/vncproxy.sock"
+    from optparse import OptionParser
 
-    logging.basicConfig(level=logging.DEBUG,
-                        format="%(levelname)s: %(message)s",
-                        datefmt="%m-%d %H:%M")
-    logging.getLogger("").name = "[master]"
+    parser = OptionParser()
+    parser.add_option("-s", "--socket", dest="ctrl_socket",
+                      help="UNIX socket path for control connections",
+                      default="/tmp/vncproxy.sock",
+                      metavar="PATH")
+    parser.add_option("-d", "--debug", action="store_true", dest="debug",
+                      help="Enable debugging information")
+    parser.add_option("-l", "--log", dest="logfile", default=None,
+                      help="Write log to FILE instead of stdout",
+                      metavar="FILE")
+    parser.add_option("-t", "--connect-timeout", dest="connect_timeout",
+                      default=30, type="int", metavar="SECONDS",
+                      help="How long to listen for clients to forward")
 
-    if os.path.exists(CTRL_SOCKET):
-        os.unlink(CTRL_SOCKET)
+    (opts, args) = parser.parse_args(sys.argv[1:])
+
+    lvl = logging.DEBUG if opts.debug else logging.INFO
+
+    logging.basicConfig(level=lvl, filename=opts.logfile,
+                        format="%(asctime)s %(levelname)s: %(message)s",
+                        datefmt="%m/%d/%Y %H:%M:%S")
+
+    if os.path.exists(opts.ctrl_socket):
+        logging.critical("Socket '%s' already exists" % opts.ctrl_socket)
+        sys.exit(1)
+
+    # TODO: make this tunable? chgrp as well?
     old_umask = os.umask(0077)
+
     ctrl = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    ctrl.bind(CTRL_SOCKET)
+    ctrl.bind(opts.ctrl_socket)
+
     os.umask(old_umask)
+
     ctrl.listen(1)
-    logging.info("Initalized, waiting for control connections")
+    logging.info("Initalized, waiting for control connections at %s" %
+                 opts.ctrl_socket)
 
     while True:
         try:
@@ -300,8 +349,14 @@ if __name__ == '__main__':
         logging.info("New control connection")
         line = client.recv(1024).strip()
         try:
+            # Control message format:
+            # <source_port>:<destination_address>:<destination_port>:<password>
+            # <password> will be used for MITM authentication of clients
+            # connecting to <source_port>, who will subsequently be forwarded
+            # to a VNC server at <destination_address>:<destination_port>
             sport, daddr, dport, password = line.split(':', 3)
-            logging.info("New forwarding [%d -> %s:%d]" % (int(sport), daddr, int(dport)))
+            logging.info("New forwarding [%d -> %s:%d]" %
+                         (int(sport), daddr, int(dport)))
         except:
             logging.warn("Malformed request: %s" % line)
             client.send("FAILED\n")
@@ -309,7 +364,8 @@ if __name__ == '__main__':
             continue
 
         client.send("OK\n")
-        VncForwarder.spawn(sport, daddr, dport, password)
+        VncForwarder.spawn(sport, daddr, dport, password, opts.connect_timeout)
         client.close()
 
+    os.unlink(opts.ctrl_socket)
     sys.exit(0)
