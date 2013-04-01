6a8190a5d5e6ad41213148a2eca4a2831a8af14d
Vangelis Koukis <vkoukis@grnet.gr>
2013-04-01 13:57:52 +0300

    Create a docs stub to keep the CI system happy

--- ci/make_docs.sh
+++ ci/make_docs.sh
@@ -2,11 +2,24 @@
 
 # Copied from main Synnefo repository
 # snf-vncauthproxy has no Sphinx-based docs yet
-exit 0
 
 set -e
 
 DOCS_DIR=$1
+
+# Make sure the $DOCS_DIR exists, otherwise
+# buildbot/common.py will throw exception
+mkdir -p $DOCS_DIR
+cat << EOF >$DOCS_DIR/index.html
+<html>
+<body>
+No docs yet.
+</body>
+</html>
+EOF
+
+echo "Created documentation stub in $(pwd)/$DOCS_DIR"
+exit 0
 
 cd docs
 make html
